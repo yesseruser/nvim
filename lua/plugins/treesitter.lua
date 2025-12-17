20e3904ea4579296e5012ed59d3391e994164328
yesseruser <yesseruseryt@gmail.com>
2025-12-17 16:27:34 +0100

    Fix nvim-treesitter compatibility

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,8 +1,9 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+  branch = "main",
 	build = ":TSUpdate",
 	config = function()
-		local config = require("nvim-treesitter.configs")
+		local config = require("nvim-treesitter.config")
 		config.setup({
 			ensure_installed = {
 				"lua",
