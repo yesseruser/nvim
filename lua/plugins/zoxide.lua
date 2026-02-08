077ed775bcba17d659efb83092a36f1126ee2e36
yesseruser <yesseruseryt@gmail.com>
2026-02-08 17:15:15 +0100

    Add keybind desc to Zoxide

--- lua/plugins/zoxide.lua
+++ lua/plugins/zoxide.lua
@@ -1,11 +1,10 @@
 return {
-  "nanotee/zoxide.vim",
-  dependencies = {
-    "junegunn/fzf",
-    "junegunn/fzf.vim",
-  },
-  config = function()
-    vim.keymap.set("n", "<leader>z", ":Zi<CR>", { silent = true })
-  end,
+	"nanotee/zoxide.vim",
+	dependencies = {
+		"junegunn/fzf",
+		"junegunn/fzf.vim",
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>z", ":Zi<CR>", { silent = true, desc = "[Z]oxide" })
+	end,
 }
-
