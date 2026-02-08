07e966ceffaa4ac0ba8e724df8a44f4fe2b1f5fe
yesseruser <yesseruseryt@gmail.com>
2026-02-08 17:05:51 +0100

    Add descriptions to telescope keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,9 +5,9 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>ae", builtin.diagnostics, {})
+			vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "[F]ind files" })
+			vim.keymap.set("n", "<leader>g", builtin.live_grep, { desc = "Live [G]rep" })
+			vim.keymap.set("n", "<leader>ae", builtin.diagnostics, { desc = "[A]ll [E]rrors" })
 		end,
 	},
 	{
