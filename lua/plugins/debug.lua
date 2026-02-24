38ee37b2c2adbfc4d0bcb602643591cdb0299bd7
yesseruser <yesseruseryt@gmail.com>
2026-02-24 22:37:15 +0100

    Add debugging config for C, C++ and Rust

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,1 +1,55 @@
-return { "mfussenegger/nvim-dap" }
+return {
+	"mfussenegger/nvim-dap",
+	config = function()
+		local dap = require("dap")
+		vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, { desc = "Toggle [D]ebug [B]reakpoint" })
+		vim.keymap.set("n", "<leader>dd", dap.continue, { desc = "Start/Continue [DD]ebugging" })
+		vim.keymap.set("n", "<leader>do", dap.step_over, { desc = "[D]ebug Step [O]ver" })
+		vim.keymap.set("n", "<leader>di", dap.step_into, { desc = "[D]ebug Step [I]nto" })
+		vim.keymap.set("n", "<leader>dr", dap.repl.open, { desc = "[D]ebug Open [R]epl" })
+
+		dap.adapters.gdb = {
+			type = "executable",
+			command = "gdb",
+			args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
+		}
+		dap.configurations.c = {
+			{
+				name = "Launch",
+				type = "gdb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				args = {}, -- provide arguments if needed
+				cwd = "${workspaceFolder}",
+				stopAtBeginningOfMainSubprogram = false,
+			},
+			{
+				name = "Select and attach to process",
+				type = "gdb",
+				request = "attach",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				pid = function()
+					local name = vim.fn.input("Executable name (filter): ")
+					return require("dap.utils").pick_process({ filter = name })
+				end,
+				cwd = "${workspaceFolder}",
+			},
+			{
+				name = "Attach to gdbserver :1234",
+				type = "gdb",
+				request = "attach",
+				target = "localhost:1234",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+			},
+		}
+		dap.configurations.cpp = dap.configurations.c
+		dap.configurations.rust = dap.configurations.c
+	end,
+}
