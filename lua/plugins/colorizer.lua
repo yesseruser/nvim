a09925a7b457b31827e7e2c1b8a4bc5d6ec99595
yesseruser <yesseruseryt@gmail.com>
2026-09-16 12:04:42 +0200

    Replace vim-colorizer with non-deprecated fork

--- lua/plugins/colorizer.lua
+++ lua/plugins/colorizer.lua
@@ -1,9 +1,9 @@
 return {
-  "norcalli/nvim-colorizer.lua",
-  config = function()
-    require("colorizer").setup({
-      css = { rgb_fn = true; };
-      'javascript';
-    }, { mode = "background" })
-  end,
+	"catgoose/nvim-colorizer.lua",
+	config = function()
+		require("colorizer").setup({
+			css = { rgb_fn = true },
+			"javascript",
+		}, { mode = "background" })
+	end,
 }
